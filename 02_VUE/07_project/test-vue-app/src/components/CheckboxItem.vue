dfdb27ea360a1f7d0d733ca249ea6251483d092d
wmh6525 <wmh6525@naver.com>
2025-03-25 17:47:39 +0900

    update

---
+++ 02_VUE/07_project/test-vue-app/src/components/CheckboxItem.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'CheckboxItem',
+  props: ['name', 'checked'],
+  data() {
+    return {
+      checked: false,
+    };
+  },
+};
+</script>
+<template>
+  <li><input type="checkbox" :checked="checked" />{{ name }}</li>
+</template>
+<style></style>
